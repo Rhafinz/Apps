a0384cc22b39a15db600cbf74084dd0141160ece
kehanggnahek <kehanggnahek@gmail.com>
2024-08-06 08:54:25 +0000

    Apps update

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/form/display_page.dart';
+import 'package:myapp/form/form_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'belajar flutter',
-      theme: ThemeData(primaryColor: Colors.blue),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  int _counter = 0;
-
-  void incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Belajar Flutter'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hitungan ke :'),
-            Text('$_counter',
-            style: TextStyle(
-              fontSize: _counter.toDouble(),
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Container(
-              child: TextButton(
-                onPressed: (){
-                  incrementCounter();
-                },
-                child: Icon(Icons.add),
-              ),
-            ),
-            Container(
-              child: TextButton(
-                onPressed: (){
-                  decrementCounter();
-                },
-                child: Icon(Icons.remove),
-                ),
-              ),
-              ],
-            ),
-          ],
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('belajar flutter'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
         ),
-      )
+        body: 
+        FormPage(),
+      ),
     );
+  }
+}
+
+class DisplayPage extends StatelessWidget {
+  const DisplayPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DisplayPage();
   }
 }
