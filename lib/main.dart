2ba1def7d8ac1b80ec90706672513474742aff2b
kehanggnahek <kehanggnahek@gmail.com>
2024-08-08 05:57:29 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/form/display_page.dart';
-import 'package:myapp/form/form_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'pages/auth/login_page.dart';
+import 'pages/auth/register_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  Future<bool> checkLoginStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token') != null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('belajar flutter'),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-        ),
-        body: 
-        FormPage(),
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
       ),
+      home: FutureBuilder<bool>(
+        future: checkLoginStatus(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.data == true) {
+            return HomePage();
+          } else {
+            return LoginPage();
+          }
+        },
+      ),
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/register': (context) => RegisterPage(),
+        '/home': (context) => HomePage(),
+      },
     );
-  }
-}
-
-class DisplayPage extends StatelessWidget {
-  const DisplayPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return DisplayPage();
   }
 }
